a3c8ff02bdeef3174e06c9726f0c747baf60b566
nahidsultan19 <nahidsultan38@gmail.com>
2022-03-24 12:18:35 +0600

    navbar completed.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className='header'>
+            <div className="navbar">
+                <a href="/logo">MealDB</a>
+                <div className="navbar-container">
+                    <a href="/home">Home</a>
+                    <a href="/meals">Meals</a>
+                    <a href="/about">About</a>
+                    <a href="/service">Service</a>
+                </div>
+            </div>
         </div>
     );
 };
