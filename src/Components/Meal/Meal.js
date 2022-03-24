c3d7d3e2212dc946dfd01a694ba0257ac2549eac
nahidsultan19 <nahidsultan38@gmail.com>
2022-03-24 15:51:14 +0600

    meals added.

--- src/Components/Meal/Meal.js
+++ src/Components/Meal/Meal.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Meal.css'
 
-const Meal = () => {
+const Meal = (props) => {
+    const { strMealThumb, strMeal, strInstructions } = props.meal;
     return (
-        <div>
-            <h1>Single Meal</h1>
+        <div className='meal'>
+            <img src={strMealThumb} alt="" />
+            <div className="meal-info">
+                <h4>{strMeal}</h4>
+                <p>{strInstructions.slice(0, 200)}</p>
+            </div>
+            <div className='cart-btn'>
+                <p>Add to Cart</p>
+            </div>
         </div>
     );
 };
--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Meal from '../Meal/Meal';
+import './Meals.css';
+
 
 const Meals = () => {
+    const [meals, setMeals] = useState([])
+    console.log(meals)
+
+    useEffect(() => {
+        fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+            .then(res => res.json())
+            .then(data => setMeals(data.meals))
+    }, [])
     return (
-        <div>
-            <h1>Meals</h1>
+
+        <div className='meals-container'>
+            <div className='meals' >
+                {
+                    Object.values(meals).map(meal => <Meal key={meal.idMeal} meal={meal}></Meal>)
+                }
+            </div>
+            <div className="order-summary">
+                <h2>Order Summary</h2>
+            </div>
         </div>
     );
 };
